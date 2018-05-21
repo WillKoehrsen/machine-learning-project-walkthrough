fe1c529217f081dfa61389c448928803cf3a0654
Will K <wjk68@case.edu>
2018-05-21 10:08:11 -0400

    Added auto-ml pipeline

--- auto_ml/tpot_exported_pipeline.py
+++ auto_ml/tpot_exported_pipeline.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.linear_model import LassoLarsCV
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import Imputer
+from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,10 +13,16 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-8.831734170906685
-exported_pipeline = GradientBoostingRegressor(alpha=0.99, learning_rate=0.1, loss="lad", max_depth=5,
-                                              max_features=0.8500000000000001, min_samples_leaf=13,
-                                              min_samples_split=14, n_estimators=100, subsample=0.6500000000000001)
+imputer = Imputer(strategy="median")
+imputer.fit(training_features)
+training_features = imputer.transform(training_features)
+testing_features = imputer.transform(testing_features)
+
+# Score on the training set was:-8.56166141363137
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=LassoLarsCV(normalize=True)),
+    GradientBoostingRegressor(alpha=0.95, learning_rate=0.1, loss="lad", max_depth=7, max_features=0.7500000000000001, min_samples_leaf=3, min_samples_split=18, n_estimators=100, subsample=0.6000000000000001)
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
